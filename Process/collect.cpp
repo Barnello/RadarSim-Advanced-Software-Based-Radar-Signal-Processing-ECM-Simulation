42dd94014d71ff91d25fde46f22f2c231e36faa0
Barnello <lucasbarn43@gmail.com>
2024-10-20 18:42:41 -0400

    switching os checkpoint

---
+++ Process/collect.cpp
@@ -0,0 +1,21 @@
+// This class will collect the data from the waveforms to store into a database and further process
+// Database: SQLLite 
+#include <iostream>
+
+// use to create database for waveforms
+// file name will be provided and databse sql functionality will be organized in this class
+class Waveform_Database{
+
+    private: 
+        const char *filename; // filename to the database 
+    
+    public: 
+        
+
+};
+
+
+int main(){
+
+    return 0;
+}
